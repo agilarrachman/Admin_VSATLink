cee8d61f3d4e7fc0f66ca2ef32ece6b1ca1b4a3a
agilarrachman <agil.musthafa11@gmail.com>
2025-12-21 21:03:51 +0700

    refactor: separate app layout from content, update primary color, and change header icon

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,79 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+        <a href="index.html" class="app-brand-link">
+            <img src="assets/img/Logo VSATLink.png" alt="logo" style="max-width: 120px">
+        </a>
+
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+        <!-- Sidebar Menu -->
+        <li class="menu-item active open">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-receipt"></i>
+                <div data-i18n="Dashboards">Orders</div>
+                <div class="badge bg-danger rounded-pill ms-auto">5</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item active">
+                    <a href="index.html" class="menu-link">
+                        <div data-i18n="Analytics">All Orders</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="index.html" class="menu-link">
+                        <div data-i18n="Analytics">Confirm Orders</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-package"></i>
+                <div data-i18n="Dashboards">Logistics</div>
+                <div class="badge bg-danger rounded-pill ms-auto">5</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="index.html" class="menu-link">
+                        <div data-i18n="Analytics">Expedition Orders</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="index.html" class="menu-link">
+                        <div data-i18n="Analytics">Pickup Orders</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-broadcast"></i>
+                <div data-i18n="Dashboards">Service Activation</div>
+                <div class="badge bg-danger rounded-pill ms-auto">5</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="index.html" class="menu-link">
+                        <div data-i18n="Analytics">All Orders</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="index.html" class="menu-link">
+                        <div data-i18n="Analytics">Confirm Orders</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="index.html" class="menu-link">
+                        <div data-i18n="Analytics">Verification Orders</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
