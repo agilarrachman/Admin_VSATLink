3d089499e9dcd1f58c3afb4fe521b64d4c347d6d
agilarrachman <agil.musthafa11@gmail.com>
2025-12-21 22:47:05 +0700

    feat: add views to display all orders and orders pending confirmation

---
+++ resources/views/orders/confirmation.blade.php
@@ -0,0 +1,183 @@
+@extends('layouts.app')
+
+@section('title', 'Admin VSATLink | Order Confirmation')
+
+@section('content')
+    <div class="container-xxl grow container-p-y">
+        <div class="card">
+            <h5 class="card-header">Order Management</h5>
+            <div class="table-responsive text-nowrap">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Order ID</th>
+                            <th>Customer</th>
+                            <th>Produk Layanan</th>
+                            <th>Tanggal Pesanan</th>
+                            <th>Detail Pesanan</th>
+                            <th>Detail Customer</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                    
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                        <tr>
+                            <td>VSL6637373</td>
+                            <td>Albert Cook</td>
+                            <td>VSATLink Aurora</td>
+                            <td>11 November 2025</td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-receipt me-2"></i>
+                                    Lihat</a></td>
+                            <td><a class="btn btn-primary" href="javascript:void(0);"><i class="bx bx-user me-2"></i>
+                                    Lihat</a></td>
+                            <td>
+                                <button type="button" class="btn btn-success">Konfirmasi</button>
+                                <button type="button" class="btn btn-danger">Batalkan</button>
+                            </td>
+                        </tr>                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('orders.index', ['management' => 'orders', 'page' => 'order-management']);
+});
+Route::get('/order-confirmation', function () {
+    return view('orders.confirmation', ['management' => 'orders', 'page' => 'order-confirmation']);
 });
 Route::get('/sign-in', function () {
     return view('signin');
