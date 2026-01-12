5543c9b4db54c87730343708a1ae0e9f77956091
agilarrachman <agil.musthafa11@gmail.com>
2026-01-12 17:38:50 +0700

    feat: add customer-received order image to order detail view

--- resources/views/logistics/index-pickup.blade.php
+++ resources/views/logistics/index-pickup.blade.php
@@ -14,13 +14,6 @@
                 </div>
             @endif
 
-            @if (session()->has('error'))
-                <div class="alert alert-danger alert-dismissible fade show mx-3" role="alert">
-                    {{ session('error') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endif
-
             <div class="table-responsive text-nowrap">
                 <table class="table">
                     <thead>
